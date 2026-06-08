8af5c160666d010f11157304ae9ab00964e2cafb
damini koranga <daminikorangadk@gmail.com>
2026-06-08 15:25:53 +0530

    Configure backend port for deployment

--- server/server.js
+++ server/server.js
@@ -100,8 +100,8 @@ app.put("/tasks/:id", (req, res) => {
 });
 
 // start server
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
 
-    console.log("Server started on port 5000");
-
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
